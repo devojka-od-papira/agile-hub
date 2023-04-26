78b70a73b7d2ddd333452fa974afe27cd6245527
Bojana <bojanabojic30@gmail.com>
2023-04-26 20:03:56 +0200

    Set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-function App() {
+import React from 'react'
+const App = () => {
   return (
     <div className="App">
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
