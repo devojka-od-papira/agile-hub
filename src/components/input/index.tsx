c9f8b623a388b41d200369879c05ae7a4d9793cc
Bojana Bojic <76129338+devojka-od-papira@users.noreply.github.com>
2023-04-27 22:01:12 +0200

    Create input component (#12)
    * Create input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,24 @@
+import React, { type FC } from 'react'
+import cx from 'classnames'
+
+import styles from './Input.module.scss'
+
+interface InputProps {
+  placeholder: string
+  id: string
+  type: string
+  classname?: string
+}
+
+const Input: FC<InputProps> = ({ placeholder, type, id, classname }) => {
+  return (
+        <input
+            id={id}
+            type={type}
+            placeholder={placeholder}
+            className={cx(styles.input, classname)}
+        />
+  )
+}
+
+export default Input
