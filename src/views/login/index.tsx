14da55da1954d41f12ed93a390142d164693a564
Bojana <bojanabojic30@gmail.com>
2023-04-30 22:09:23 +0200

    Create water drops animation for log in view

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+
+import styles from './LogIn.module.scss'
 const Login = () => {
   return (
-        <div>
-         log in
+        <div className={styles.wrapper}>
+            <div className={styles.drops}>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+                <div className={styles.drop}></div>
+            </div>
         </div>
   )
 }
